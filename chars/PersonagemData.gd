6f39c6452d62579e7dc5d2e01c85130d2aa68a69
HenriqueFernandes-br <rick.fernandes.dev@gmail.com>
2026-09-15 20:13:04 -0300

    Char and Base Stats Init

---
+++ chars/PersonagemData.gd
@@ -0,0 +1,39 @@
+extends Resource
+class_name PersonagemData
+
+@export var vida_max: int = 100
+
+@export var regeneracao: float = 0.0
+
+@export var armadura: int = 0
+@export var armadura_max: int = 50
+
+@export var velocidade: float = 1
+
+@export var forca: float = 1
+@export var forca_max: float = 10
+
+@export var area: float = 1
+@export var area_max: float = 10
+
+@export var agilidade: float = 1
+@export var agilidade_max: float = 5
+
+@export var duracao: float = 1
+@export var duracao_max: float = 5
+
+@export var quantidade: int = 0
+@export var quantidade_max: int = 10
+
+@export var cooldown: float = 1
+@export var cooldown_min: float = 0.1
+
+@export var sorte: float = 1
+
+@export var crescimento: float = 1
+
+@export var ganancia: float = 1
+
+@export var maldicao: float = 1
+
+@export var magnetismo: int = 30
